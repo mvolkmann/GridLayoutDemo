c7688606c2d8b93047d1078b14823f6f3a4238c2
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-10-07 15:50:23 -0500

    Initial Commit

---
+++ GridLayoutDemo/GridLayoutDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridLayoutDemoApp.swift
+//  GridLayoutDemo
+//
+//  Created by Mark Volkmann on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct GridLayoutDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
